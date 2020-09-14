47e0d10316888964c784125da815cb420bb958a7
Egor Vasyakin <e.vasyakin@itevas.ru>
2020-09-14 20:52:17 +0300

    Update AuthException.php

--- src/AuthException.php
+++ src/AuthException.php
@@ -4,6 +4,8 @@
  */
 namespace Evas\Auth;
 
+use Evas\Auth\AuthAdapter;
+
 /**
  * Класс исключений авторизации.
  * @author Egor Vayakin <egor@evas-php.com>
@@ -11,4 +13,17 @@ namespace Evas\Auth;
  */
 class AuthException extends \Exception
 {
+    /**
+     * Переопределяем конструктор.
+     * @param string текст сообщения
+     * @param int код исключения
+     * @param \Throwable|null предыдущее исключение
+     */
+    public function __construct(string $message = '', int $code = 0, \Throwable $previous = null)
+    {
+        if (empty($message) && !empty($code)) {
+            $message = AuthAdapter::ERRORS_MAP[$code] ?? '';
+        }
+        return parent::__construct($message, $code, $previous)
+    }
 }
