51e505396a5e07fc97f7d4f6e6d0839fc441f5ce
Egor Vasyakin <e.vasyakin@itevas.ru>
2020-09-14 17:25:44 +0300

    Create AuthException.php

---
+++ src/AuthException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package evas-php\evas-auth
+ */
+namespace Evas\Auth;
+
+/**
+ * Класс исключений авторизации.
+ * @author Egor Vayakin <egor@evas-php.com>
+ * @since 3 Sep 2020
+ */
+class AuthException extends \Exception
+{
+}
