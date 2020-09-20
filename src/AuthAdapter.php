54df309e3262f562620fb07a3a7f18e69bc31e39
Egor Vasyakin <e.vasyakin@itevas.ru>
2020-09-21 01:05:48 +0300

    Update AuthAdapter.php

--- src/AuthAdapter.php
+++ src/AuthAdapter.php
@@ -5,12 +5,8 @@
 namespace Evas\Auth;
 
 use Evas\Auth\AuthException;
-use Evas\Auth\Models\AuthGrant;
-use Evas\Auth\Models\AuthGrantConfirm;
-use Evas\Auth\Models\AuthSession;
-use Evas\Auth\Sources\Vk\VkController;
-use Evas\Auth\Sources\Fb\FbController;
-use Evas\Auth\Sources\Google\GoogleController;
+use Evas\Auth\Helpers\Config;
+use Evas\Base\App;
 
 /**
  * Контроллер авторизации.
@@ -30,62 +26,128 @@ class AuthAdapter
     const ERROR_USER_FAIL_PASSWORD = 5;
     const ERROR_VALIDATOR = 6;
     const ERROR_USER_ALREADY_EXISTS = 7;
-    const ERROR_AUTH_GRANT_CONFIRM_NOT_FOUND = 8;
+    const ERROR_AUTH_GRANT_NOT_FOUND = 8;
+    const ERROR_AUTH_GRANT_CONFIRM_NOT_FOUND = 9;
+
+    // /**
+    //  * @static array маппинг ошибок
+    //  */
+    // const ERRORS_MAP = [
+    //     self::ERROR_SOURCE_CONTROLLER_NOT_SUPPORTED => 'Обработчик авторизации через %s не поддерживается',
+    //     self::ERROR_SOURCE_CONTROLLER_NOT_FOUND => 'Обработчик авторизации через %s не найден',
+    //     self::ERROR_SOURCE_CONTROLLER_ACTION_NOT_FOUND => 'Действие %s обработчика авторизации через %s не найдено',
+    //     self::ERROR_MODEL_TABLE_NOT_FOUND => 'Имя таблицы модели данных %s не найдено',
+    //     self::ERROR_USER_NOT_FOUND => 'Пользователь не найден',
+    //     self::ERROR_USER_FAIL_PASSWORD => 'Неверный пароль',
+    //     self::ERROR_VALIDATOR => 'Неверное имя пользователя/пароль',
+    //     self::ERROR_USER_ALREADY_EXISTS => 'Пользователь уже существует',
+    //     self::ERROR_AUTH_GRANT_CONFIRM_NOT_FOUND => 'Код подтверждения не найден, возможно, вы уже подтвердили вход',
+    // ];
 
     /**
-     * @static array маппинг ошибок
+     * @var Config
      */
-    const ERRORS_MAP = [
-        self::ERROR_SOURCE_CONTROLLER_NOT_SUPPORTED => 'Обработчик авторизации через %s не поддерживается',
-        self::ERROR_SOURCE_CONTROLLER_NOT_FOUND => 'Обработчик авторизации через %s не найден',
-        self::ERROR_SOURCE_CONTROLLER_ACTION_NOT_FOUND => 'Действие %s обработчика авторизации через %s не найдено',
-        self::ERROR_MODEL_TABLE_NOT_FOUND => 'Имя таблицы модели данных %s не найдено',
-        self::ERROR_USER_NOT_FOUND => 'Пользователь не найден',
-        self::ERROR_USER_FAIL_PASSWORD => 'Неверный пароль',
-        self::ERROR_VALIDATOR => 'Неверное имя пользователя/пароль',
-        self::ERROR_USER_ALREADY_EXISTS => 'Пользователь уже существует',
-        self::ERROR_AUTH_GRANT_CONFIRM_NOT_FOUND => 'Код подтверждения не найден, возможно, вы уже подтвердили вход',
-    ];
+    protected static $config;
 
     /**
-     * @static array маппинг статусов грантов авторизации
+     * @var Database
      */
-    const AUTH_GRANT_STATUSES_MAP = [
-        AuthGrant::STATUS_NOT_CONFIRMED => 'не подтвержден',
-        AuthGrant::STATUS_CONFIRMED => 'подвержден',
-        AuthGrant::STATUS_OUTDATED => 'необходимо обновить',
-    ];
+    protected static $db;
 
     /**
-     * @static array маппинг таблиц моделей
+     * Путь к дефолтному конфигу адаптера.
      */
-    const MODELS_TABLES = [
-        AuthGrant::class => 'auth_grants',
-        AuthSession::class => 'auth_sessions',
-        AuthGrantConfirm::class => 'auth_grant_confirms',
-    ];
-
+    const DEFAULT_CONFIG_PATH = __DIR__ . '/config.default.php';
 
     /**
-     * @static string имя токена авторизации в cookie
+     * Путь к кастомному конфигу адаптера.
      */
-    const AUTH_TOKEN_COOKIE_NAME = 'token';
+    public static $customConfigPath;
 
     /**
-     * @static int время жизни токена авторизации в секундах
+     * Получение конфига адаптера.
+     * @return Config
      */
-    const AUTH_TOKEN_ALIVE = 2592000;
+    public static function config(): Config
+    {
+        if (empty(static::$config)) {
+            $defaultConfig = include static::DEFAULT_CONFIG_PATH;
+            static::$config = new Config(static::$customConfigPath, $defaultConfig);
+        }
+        return static::$config;
+    }
 
     /**
-     * @var array маппинг источников авторизации.
+     * Получение элемента по индексу из списка в конфиге.
+     * @param string имя списка
+     * @param string|int индекс элемента списка
+     * @return mixed
      */
-    protected static $sources = [
-        'vk' => VkController::class,
-        'fb' => FbController::class,
-        'google' => GoogleController::class,
-        'email' => EmailController::class,
-        'phone' => PhoneController::class,
-    ];
+    public static function getConfigListItem(string $listName, $itemIndex)
+    {
+        $list = static::config()->get($listName) ?? [];
+        return $list[$itemIndex] ?? null;
+    }
+
+
+    // /**
+    //  * @static array маппинг статусов грантов авторизации
+    //  */
+    // const AUTH_GRANT_STATUSES_MAP = [
+    //     AuthGrant::STATUS_NOT_CONFIRMED => 'не подтвержден',
+    //     AuthGrant::STATUS_CONFIRMED => 'подвержден',
+    //     AuthGrant::STATUS_OUTDATED => 'необходимо обновить',
+    // ];
+
+    // /**
+    //  * @static array маппинг таблиц моделей
+    //  */
+    // const MODELS_TABLES = [
+    //     AuthGrant::class => 'auth_grants',
+    //     AuthSession::class => 'auth_sessions',
+    //     AuthGrantConfirm::class => 'auth_grant_confirms',
+    // ];
+
+
+    // /**
+    //  * @static string имя токена авторизации в cookie
+    //  */
+    // const AUTH_TOKEN_COOKIE_NAME = 'token';
+
+    // /**
+    //  * @static int время жизни токена авторизации в секундах
+    //  */
+    // const AUTH_TOKEN_ALIVE = 2592000;
+
+    // /**
+    //  * @var array маппинг источников авторизации.
+    //  */
+    // protected static $sources = [
+    //     'vk' => VkController::class,
+    //     'fb' => FbController::class,
+    //     'google' => GoogleController::class,
+    //     'email' => EmailController::class,
+    //     'phone' => PhoneController::class,
+    // ];
+
+    /**
+     * Установка соединения с базой данных.
+     * @param Database
+     */
+    public static function setDb(Database &$db)
+    {
+        static::$db = &$db;
+    }
+
+    /**
+     * Получение соединения с базой данных.
+     * @return Database
+     */
+    public static function getDb(): Database
+    {
+        return static::$db;
+        // return App::getDb();
+    }
 
     /**
      * Запуск обработчика.
@@ -96,7 +158,15 @@ class AuthAdapter
      */
     public static function run(string $source, string $action, array $params = null)
     {
-        $controllerClass = static::$sources[$source] ?? null;
+        $arr1 = ['a' => ['x' => 1, 'y' => 2], 'b' => 2];
+        $arr2 = ['a' => ['z' => 3], 'c' => 4];
+        $arr3 = array_merge_recursive($arr1, $arr2);
+        var_dump($arr3);
+        exit();
+        // $controllerClass = static::$sources[$source] ?? null;
+        // $sources = static::config()->get('sources') ?? [];
+        // $controllerClass = $sources[$source] ?? null;
+        $controllerClass = static::getConfigListItem('sources', $source);
         if (empty($controllerClass)) {
             static::throwError(static::ERROR_SOURCE_CONTROLLER_NOT_SUPPORTED, [$source]);
         }
@@ -119,7 +189,10 @@ class AuthAdapter
      */
     public static function throwError(int $code, array $props = null)
     {
-        $error = static::ERRORS_MAP[$code];
+        // $error = static::ERRORS_MAP[$code];
+        // $errorsMap = static::config()->get('errors_map') ?? [];
+        // $message = $errorsMap[$code] ?? '';
+        $error = static::getConfigListItem('errors_map', $code);
         if (!empty($props)) {
             $error = vsprintf($error, $props);
         }
@@ -134,7 +207,10 @@ class AuthAdapter
      */
     public static function getModelTableName(string $className): string
     {
-        $tableName = static::MODELS_TABLES[$className] ?? null;
+        // $tableName = static::MODELS_TABLES[$className] ?? null;
+        // $modelsTables = static::config()->get('models_tables') ?? [];
+        // $tableName = $modelsTables[$className] ?? null;
+        $tableName = static::getConfigListItem('models_tables', $className);
         if (empty($tableName)) {
             static::throwError(static::ERROR_MODEL_TABLE_NOT_FOUND, [$className]);
         }
