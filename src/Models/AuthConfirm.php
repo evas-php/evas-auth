bf23fce3f28227ab86c46f2b2eec7071177b98cb
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-08-08 05:54:54 +0300

    Create AuthConfirm.php

---
+++ src/Models/AuthConfirm.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Модель подтверждения телефона/email.
+ * @package evas-php\evas-auth
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Auth\Models;
+
+use Evas\Auth\AuthException;
+use Evas\Auth\Help\Model;
+
+class AuthConfirm extends Model
+{
+    const TYPE_EMAIL = 0;
+    const TYPE_PHONE = 1;
+    const TYPES = [
+        self::TYPE_EMAIL => 'email',
+        self::TYPE_PHONE => 'phone',
+    ];
+
+    /** @var int id подтверждения */
+    public $id;
+    /** @var int id пользователя */
+    public $user_id;
+    /** @var int тип источника получения */
+    public $type;
+    /** @var string источник получения кода подтверждения */
+    public $to;
+    /** @var string код подтверждения */
+    public $code;
+    /** @var string время создания */
+    public $create_time;
+    /** @var string время просрочки */
+    public $end_time;
+    /** @var string время завершения */
+    public $complete_time;
+
+
+    /**
+     * Проверка просроченности подтверждения.
+     * @return bool
+     */
+    public function isOutdated(): bool
+    {
+        return time() > strtotime($this->end_time);
+    }
+
+    /**
+     * Установка завершения подтверждения.
+     * @throws AuthException
+     */
+    public function complete()
+    {
+        if ($this->isOutdated()) {
+            throw new AuthException('Код подтверждения устарел');
+        }
+        $this->complete_time = date('Y-m-d H:i:s');
+        $this->save();
+    }
+
+    /**
+     * Создание восстановления через email.
+     * @param int id пользователя
+     * @param string email
+     * @return static
+     */    
+    public static function createToEmail(int $user_id, string $to)
+    {
+        $code = static::generateCode();
+        $data = compact('user_id', 'to', 'code');
+        $data['type'] = self::TYPE_EMAIL;
+        return new static($data);
+    }
+
+    /**
+     * Создание восстановления через телефон.
+     * @param int id пользователя
+     * @param string номер телефона
+     * @return static
+     */   
+    public static function createToPhone(int $user_id, string $to)
+    {
+        $code = static::generateCode();
+        $data = compact('user_id', 'to', 'code');
+        $data['type'] = self::TYPE_PHONE;
+        return new static($data);
+    }
+
+    /**
+     * Генерация кода для восстановления.
+     * @return string
+     */
+    public static function generateCode(): string
+    {
+        return '';
+    }
+
+    /**
+     * Поиск по коду отправленному на email.
+     * @param string код
+     * @return static|null
+     */
+    public static function findByEmailCode(string $code): ?AuthRecovery
+    {
+        return static::findByTypeCode(self::TYPE_EMAIL, $code);
+    }
+
+    /**
+     * Поиск по коду отправленному на телефон.
+     * @param string код
+     * @return static|null
+     */
+    public static function findByPhoneCode(string $code): ?AuthRecovery
+    {
+        return static::findByTypeCode(self::TYPE_PHONE, $code);
+    }
+
+    /**
+     * Поиск по коду и типу отправки.
+     * @param string тип отправки кода
+     * @param string код
+     * @return static|null
+     */
+    public static function findByTypeCode(int $type, string $code): ?AuthRecovery
+    {
+        return static::find()->where(
+            '`type` = ? AND `code` = ?', [$type, $code]
+        )->one()->classObject(static::class);
+    }
+}
