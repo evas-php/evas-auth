c4cdc80c8651f84530fdf896bd72f0baf001f2c5
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-08-08 05:55:18 +0300

    Update config.default.php

--- src/config.default.php
+++ src/config.default.php
@@ -1,61 +1,82 @@
 <?php
 /**
  * Дефолтный конфиг модуля авторизации.
+ * @package evas-php\evas-auth
  * @author Egor Vasyakin <egor@evas-php.com>
- * @since 16 Sep 2020
  */
 
-use Evas\Auth\AuthAdapter as AA;
+use Evas\Auth\Auth as A;
+use Evas\Auth\Models\AuthConfirm;
 use Evas\Auth\Models\AuthGrant;
-use Evas\Auth\Models\AuthGrantConfirm;
-use Evas\Auth\Models\AuthGrantRecovery;
+use Evas\Auth\Models\AuthRecovery;
 use Evas\Auth\Models\AuthSession;
-use Evas\Auth\Sources\Email\EmailController;
-use Evas\Auth\Sources\Google\GoogleController;
-use Evas\Auth\Sources\Fb\FbController;
-use Evas\Auth\Sources\Phone\PhoneController;
-use Evas\Auth\Sources\Vk\VkController;
+
+use Evas\Auth\Sources\FbOauth;
+use Evas\Auth\Sources\GoogleOauth;
+use Evas\Auth\Sources\VkOauth;
+
 use Evas\Base\App;
 
 return [
-    'errors_map' => [
-        AA::ERROR_SOURCE_CONTROLLER_NOT_SUPPORTED => 'Обработчик авторизации через %s не поддерживается',
-        AA::ERROR_SOURCE_CONTROLLER_NOT_FOUND => 'Обработчик авторизации через %s не найден',
-        AA::ERROR_SOURCE_CONTROLLER_ACTION_NOT_FOUND => 'Действие %s обработчика авторизации через %s не найдено',
-        AA::ERROR_MODEL_TABLE_NOT_FOUND => 'Имя таблицы модели данных %s не найдено',
-        AA::ERROR_USER_NOT_FOUND => 'Пользователь не найден',
-        AA::ERROR_USER_FAIL_PASSWORD => 'Неверный пароль',
-        AA::ERROR_VALIDATOR => 'Неверное имя пользователя/пароль',
-        AA::ERROR_USER_ALREADY_EXISTS => 'Пользователь уже существует',
-        AA::ERROR_AUTH_GRANT_NOT_FOUND => 'Способ входа не найден',
-        AA::ERROR_AUTH_GRANT_CONFIRM_NOT_FOUND => 'Способ входа не найден или уже подтвержден',
-        AA::ERROR_AUTH_GRANT_RECOVERY_NOT_FOUND => 'Восстановление доступа с таким кодом не найдено',
+    'errors' => [
+        'auth_not_supported' => 'Способ авторизации через %s не поддерживается',
+        'oauth_handler_not_setted' => 'Обработчик авторизации через %s не установлен',
+        'oauth_config_not_setted' => 'Конфиг авторизации через %s не установлен',
+        'user_not_found' => 'Пользователь не найден',
+        'user_fail_password' => 'Неверный логин или пароль',
+        'user_already_exists' => 'Пользователь уже существует. %s занят',
+        'grant_not_found' => 'Способ входа не найден',
+        'confirm_not_found' => 'Способ входа не найден или уже подтверждён',
+        'recovery_not_found' => 'Восстановление доступа с таким кодом не найдено',
+        'oauth_code_empty' => '%s не передал ключ доступа',
+        'oauth_empty_access' => '%s не передал права доступа',
+        'oauth_error_response' => '%s ответил ошибкой: %s',
+        'oauth_empty_user_data' => '%s не передал данные пользователя',
+        'oauth_fetch_url_error' => 'Не удаётся получить данные по url: %s',
+        'model_table_not_exists' => 'Имя таблицы модели данных %s не установлено',
+        'token_exceeed_max_try_generated' => 'Превышено максимальное количество попыток проверки на уникальность токена',
+        // A::ERROR_SOURCE_CONTROLLER_NOT_SUPPORTED => 'Обработчик авторизации через %s не поддерживается',
+        // A::ERROR_SOURCE_CONTROLLER_NOT_FOUND => 'Обработчик авторизации через %s не найден',
+        // A::ERROR_SOURCE_CONTROLLER_ACTION_NOT_FOUND => 'Действие %s обработчика авторизации через %s не найдено',
+        // A::ERROR_MODEL_TABLE_NOT_FOUND => 'Имя таблицы модели данных %s не найдено',
+        // A::ERROR_USER_NOT_FOUND => 'Пользователь не найден',
+        // A::ERROR_USER_FAIL_PASSWORD => 'Неверный пароль',
+        // A::ERROR_VALIDATOR => 'Неверное имя пользователя/пароль',
+        // A::ERROR_USER_ALREADY_EXISTS => 'Пользователь уже существует',
+        // A::ERROR_AUTH_GRANT_NOT_FOUND => 'Способ входа не найден',
+        // A::ERROR_AUTH_GRANT_CONFIRM_NOT_FOUND => 'Способ входа не найден или уже подтвержден',
+        // A::ERROR_AUTH_GRANT_RECOVERY_NOT_FOUND => 'Восстановление доступа с таким кодом не найдено',
     ],
     'auth_grant_status_string_format' => 'Вход через %s %s',
-    'auth_grant_statuses_map' => [
-        AuthGrant::STATUS_UNKNOWN => 'с неизвестным статусом',
-        AuthGrant::STATUS_INIT => 'не подтвержден',
-        AuthGrant::STATUS_CONFIRMED => 'подвержден',
-        AuthGrant::STATUS_UNACTIVE => 'выключен',
-        AuthGrant::STATUS_OUTDATED => 'необходимо обновить',
+    // 'auth_grant_statuses' => [
+    //     AuthGrant::STATUS_UNKNOWN => 'с неизвестным статусом',
+    //     AuthGrant::STATUS_INIT => 'не подтвержден',
+    //     AuthGrant::STATUS_CONFIRMED => 'подвержден',
+    //     AuthGrant::STATUS_UNACTIVE => 'выключен',
+    //     AuthGrant::STATUS_OUTDATED => 'необходимо обновить',
+    // ],
+    'models_to_tables' => [
+        AuthGrant::class => 'auth_grant',
+        AuthSession::class => 'auth_session',
+        AuthConfirm::class => 'auth_confirm',
+        AuthRecovery::class => 'auth_recovery',
     ],
-    'models_tables' => [
-        AuthGrant::class => 'auth_grants',
-        AuthSession::class => 'auth_sessions',
-        AuthGrantConfirm::class => 'auth_grant_confirm',
-        AuthGrantRecovery::class => 'auth_grant_recovery',
+    'password_enable' => true,
+    'foreignClasses' => [
+        'fb' => FbOauth::class,
+        'google' => GoogleOauth::class,
+        'vk' => VkOauth::class,
     ],
-    'sources' => [
-        'vk' => VkController::class,
-        'facebook' => FbController::class,
-        'google' => GoogleController::class,
-        'email' => EmailController::class,
-        'phone' => PhoneController::class,
+    'foreigns' => [
+        'fb' => [],
+        'google' => [],
+        'vk' => [],
     ],
-    'supported_sources' => ['vk', 'facebook', 'google', 'email', 'phone'],
-    'auth_token_cookie_name' => 'token',
-    'auth_token_alive' => 2592000,
-    'database' => function () {
-        return App::getDb();
+    'user_models' => [],
+    'token_cookie_name' => 'token',
+    'token_alive' => 2592000,
+    'token_generate_max_tries' => 20,
+    'db' => function (string $dbname = null) {
+        return App::db($dbname);
     },
 ];
