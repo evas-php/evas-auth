a8758d71f1c2947bbdbc73d63c4f47b84bf34653
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-08-08 05:54:41 +0300

    Create LoginUserInterface.php

---
+++ src/Interfaces/LoginUserInterface.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Интерфейс пользователя для авторизации.
+ * @package evas-php\evas-auth
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Auth\Interfaces;
+
+interface LoginUserInterface
+{
+    /**
+     * Добавление пользователя по внешней авторизации.
+     * @param string источник
+     * @param array данные пользователя
+     */
+    public static function insertByForeign(string $source, array $data);
+
+    /**
+     * Добавление пользователя по паролю.
+     * @param array данные пользователя
+     */
+    public static function insertByPassword(array $data);
+
+    /**
+     * Валидация для входа по паролю.
+     * @param array данные запроса
+     * @return aray отвалидированные данные
+     */
+    public static function validateLogin(array $payload): array;
+
+    /**
+     * Валидация для регистрации по паролю.
+     * @param array данные запроса
+     * @return aray отвалидированные данные
+     */
+    public static function validateRegistration(array $payload): array;
+
+    /**
+     * Получение уникальных ключей пользователя для авторизации по паролю.
+     * @return array
+     */
+    public static function uniqueKeys(): array;
+
+    /**
+     * Получение имени уникального ключа пользователя.
+     * @param string ключ
+     * @return string|null имя ключа
+     */
+    public static function getUniqueKeyLabel(string $name): ?string;
+
+    /**
+     * Поиск записи по уникальным ключам для регистрации или входа по паролю.
+     * @param array данные
+     * @return static|null
+     */
+    public static function findByUniqueKeys(array $data);
+}
