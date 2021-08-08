ee4d9f6e5df5a50bf2fc1b4f129d0868376348d3
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-08-08 05:53:56 +0300

    Create Model.php

---
+++ src/Help/Model.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Базовая абстрактная модель данных модуля авторизации.
+ * @package evas-php\evas-auth
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Auth\Help;
+
+use Evas\Auth\Auth;
+use Evas\Db\Interfaces\DatabaseInterface;
+use Evas\Orm\ActiveRecord;
+
+abstract class Model extends ActiveRecord
+{
+     /**
+     * Переопределяем получение имени таблицы.
+     * @return string
+     */
+    public static function tableName(): string
+    {
+        return Auth::getModelTableName(static::class);
+    }
+
+    /**
+     * Получение соединения с базой данных.
+     * @param bool использовать ли соединение для записи
+     * @return DatabaseInterface
+     */
+    public static function getDb(bool $write = false): DatabaseInterface
+    {
+        $dbname = static::getDbName($write);
+        return Auth::getDb($dbname);
+        // return Auth::getDb($write);
+    }
+}
