53bed722722fff8d223f865897a293ffac6cd287
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-08-14 17:17:05 +0300

    Update AuthConfirm.php

--- src/Models/AuthConfirm.php
+++ src/Models/AuthConfirm.php
@@ -6,15 +6,17 @@
  */
 namespace Evas\Auth\Models;
 
+use Evas\Auth\Auth;
 use Evas\Auth\AuthException;
 use Evas\Auth\Help\Model;
 use Evas\Auth\Help\Token;
 
 class AuthConfirm extends Model
 {
-    const TYPE_EMAIL = 0;
-    const TYPE_PHONE = 1;
+    const TYPE_EMAIL = 1;
+    const TYPE_PHONE = 2;
     const TYPES = [
+        null => 'unknown',
         self::TYPE_EMAIL => 'email',
         self::TYPE_PHONE => 'phone',
     ];
@@ -57,17 +59,35 @@ class AuthConfirm extends Model
         $this->save();
     }
 
+
+    /**
+     * Определение типа источника получения кода.
+     * @param string источник получения
+     * @return int|null тип источника
+     */
+    public static function getRecipientType(string $to): ?int
+    {
+        return (new EmailField)->checkPattern($to) ? self::TYPE_EMAIL 
+        : (
+            (new PhoneField)->checkPattern($to) ? self::TYPE_PHONE
+            : null
+        );
+    }
+
     /**
      * Создание подтверждения.
      * @param int id пользователя
      * @param string источник получения
+     * @param string|null тип источника получения
      * @return static
      */
-    public static function make(int $user_id, string $to)
+    public static function make(int $user_id, string $to, string $type = null)
     {
         $code = static::generateCode();
-        $type = self::TYPE_EMAIL;
-        $data = compact('user_id', 'to', 'code', 'type');
+        if ($type) $type = array_search($type, self::TYPES) ?? null;
+        else $type = static::getRecipientType($to);
+        $end_time = date('Y-m-d H:i:s', time() + (int) Auth::config()['code_alive']);
+        $data = compact('user_id', 'to', 'code', 'type', 'end_time');
         return static::insert($data);
     }
 
@@ -77,7 +97,13 @@ class AuthConfirm extends Model
      */
     public static function generateCode(): string
     {
-        return Token::generateUniqueIn([static::tableName(), 'code'], 6);
+        // $codeLength = Auth::config()['code_length'] ?? 6;
+        // return Token::generateUniqueIn([static::tableName(), 'code'], $codeLength);
+        return (new Token([
+            'symbols' => '0123456789',
+            'token_length' => Auth::config()['code_length'],
+            'token_generate_max_tries' => Auth::config()['token_generate_max_tries'],
+        ]))->generateUniqueIn([static::tableName(), 'code']);
     }
 
     /**
